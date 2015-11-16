9a47f7b72ef4f232523a2ee1435a076cb601e5d2
Ionut C. Herciu <I.C.Herciu@bradford.ac.uk>
2015-11-16 22:28:39 +0000

    Merge-ish

---
+++ app/Http/Middleware/AdminRequired.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminRequired
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Check if a user is logged in.
+        if (!$user = $request->user())
+            return $next($request);
+
+        if ($user->isAdmin)
+            return $next($request);
+
+        return abort(403);
+    }
+}
