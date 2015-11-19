fffd5070130d4caa0f563baf2986c3cc6eff458d
Andu Zorilă <Andu Zoril>
2015-11-19 23:02:46 +0000

    Update GUI

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,5 +1,46 @@
-<form method="POST" action="email">
-    {!! csrf_field() !!}
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>GMS Login</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{asset('plugins/iCheck/square/blue.css')}}">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  @if ($message = Session::get('message'))
+  @endif
+    @if ($errors = Session::get('errors'))
+        @foreach ($errors->all() as $error)
+           
+        @endforeach
+    @endif
+  </head>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a><b>Garage</b>Management System</a>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">Send email to reset the password</p>
+        <form action="email" method="post">
+        {!! csrf_field() !!}
 
     @if (count($errors) > 0)
         <ul>
@@ -8,15 +49,35 @@
             @endforeach
         </ul>
     @endif
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Send</button>
+            </div><!-- /.col -->
+          </div>
+          </div>
+        </form>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+        </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
 
-    <div>
-        <button type="submit">
-            Send Password Reset Link
-        </button>
-    </div>
-</form>
+    <!-- jQuery 2.1.4 -->
+    <script src="../../plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <!-- iCheck -->
+    <script src="../../plugins/iCheck/icheck.min.js"></script>
+    <script>
+      $(function () {
+        $('input').iCheck({
+          checkboxClass: 'icheckbox_square-blue',
+          radioClass: 'iradio_square-blue',
+          increaseArea: '20%' // optional
+        });
+      });
+    </script>
+  </body>
+</html>
